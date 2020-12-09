28340759e48099ec5f909d65545f5bdef78212c2
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-12-09 14:11:45 -0500

    parallax template add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   resources :templates
 
-  root to: redirect('/templates')
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
